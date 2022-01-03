8924b665d1ba3882a405660f23e8fbc4b1c74de2
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-03 13:09:10 +0700

    add:query_param_test

---
+++ query_param_test.go
@@ -0,0 +1,32 @@
+package Golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SayHello(writer http.ResponseWriter, request *http.Request) {
+	//mendapatkan data seluruh url
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprint(writer, "Hello")
+	} else {
+		//Fprintf mendukung formater
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?name=Galih", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
