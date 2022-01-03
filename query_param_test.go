2e24c00f568371002d78521b4584f9a1de78c3af
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-03 13:22:24 +0700

    feat:query_param_test add MultipleQueryParameter, TestMultipleQueryParameter

--- query_param_test.go
+++ query_param_test.go
@@ -30,3 +30,22 @@ func TestQueryParameter(t *testing.T) {
 
 	fmt.Println(string(body))
 }
+
+func MultipleQueryParameter(w http.ResponseWriter, r *http.Request) {
+	firstName := r.URL.Query().Get("first_name")
+	lastName := r.URL.Query().Get("last_name")
+
+	fmt.Fprintf(w, "Hello %s %s", firstName, lastName)
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?first_name=Galih&last_name=Setiadi", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleQueryParameter(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
