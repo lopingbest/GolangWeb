cd7c538d5052abde82de31afca43d0aae0776ad0
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-03 16:59:41 +0700

    feat:header_test add RequestHeades, TestRequestHeader, ResponseHeader

---
+++ header_test.go
@@ -0,0 +1,34 @@
+package Golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func RequestHeader(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("content-type")
+	fmt.Fprint(w, contentType)
+}
+
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", nil)
+	//case in sensitive, bebas besar kecil "content-type"
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func ResponseHeader(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("X-Powered-By", "Telkom Indonesia")
+	fmt.Fprint(w, "OK")
+}
