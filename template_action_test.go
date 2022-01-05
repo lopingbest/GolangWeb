e6f0d56ce397ea991ca0e9246b58a5467803f4f4
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-05 14:26:34 +0700

    feat:template_action_test add TemplateActionOperator,TestTemplateActionOperator

--- template_action_test.go
+++ template_action_test.go
@@ -26,3 +26,21 @@ func TestTemplateActionIf(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+func TemplateActionOperator(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./template/comparator.gohtml"))
+	t.ExecuteTemplate(w, "comparator.gohtml", map[string]interface{}{
+		"Title":      "Template Data Struct",
+		"FinalValue": 90,
+	})
+}
+
+func TestTemplateActionOperator(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionOperator(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
