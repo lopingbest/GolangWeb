f30d39cbde787d7b323b44f9aa7e799613a016e0
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-07 10:59:52 +0700

    add:xss_test

---
+++ xss_test.go
@@ -0,0 +1,36 @@
+package Golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateAutoEscape(w http.ResponseWriter, r *http.Request) {
+	myTemplates.ExecuteTemplate(w, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto Escape",
+		"Body":  "<p> ini adalah body <script>alert(`Anda Kami Retas`)</script></p>",
+	})
+}
+func TestTemplateAutoEscape(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscape(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateAutoEscape),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
