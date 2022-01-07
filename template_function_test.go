b18a230a11f03c043a0a9fb87d6b9d49911e7262
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-07 09:46:27 +0700

    add:template_funtion_test

---
+++ template_function_test.go
@@ -0,0 +1,35 @@
+package Golang_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type MyPage struct {
+	Name string
+}
+
+func (myPage MyPage) SayHello(name string) string {
+	return "Hello " + name + ",My Name Is " + myPage.Name
+}
+
+func TemplateFunction(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{.SayHello "Galih"}}`))
+	t.ExecuteTemplate(w, "FUNCTION", MyPage{
+		Name: "Galih",
+	})
+}
+
+func TestTemplateFunction(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunction(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
