747c73f13c031ecbcdf5bb4d313a7d04e1b4305a
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-07 11:05:50 +0700

    feat:xss_template

--- xss_test.go
+++ xss_test.go
@@ -2,6 +2,7 @@ package Golang_web
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -14,6 +15,7 @@ func TemplateAutoEscape(w http.ResponseWriter, r *http.Request) {
 		"Body":  "<p> ini adalah body <script>alert(`Anda Kami Retas`)</script></p>",
 	})
 }
+
 func TestTemplateAutoEscape(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
 	recorder := httptest.NewRecorder()
@@ -28,6 +30,34 @@ func TestTemplateAutoEscapeServer(t *testing.T) {
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: http.HandlerFunc(TemplateAutoEscape),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TemplateAutoEscapeDisabled(w http.ResponseWriter, r *http.Request) {
+	myTemplates.ExecuteTemplate(w, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto Escape",
+		"Body":  template.HTML("<p> ini adalah body</p>"),
+	})
+}
+
+func TestTemplateAutoEscapeDisabled(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscapeDisabled(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeDisabledServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateAutoEscapeDisabled),
 	}
 	err := server.ListenAndServe()
 	if err != nil {
