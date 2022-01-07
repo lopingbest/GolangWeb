42c475c112fac6c3a02ad88fa021091f1485aa55
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-07 17:14:58 +0700

    feat:xss_test add TemplateXSS

--- xss_test.go
+++ xss_test.go
@@ -64,3 +64,31 @@ func TestTemplateAutoEscapeDisabledServer(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TemplateXSS(w http.ResponseWriter, r *http.Request) {
+	myTemplates.ExecuteTemplate(w, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto Escape",
+		"Body":  template.HTML(r.URL.Query().Get("body")),
+	})
+}
+
+func TestTemplateXSS(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/?body=<p>alert</p>", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateXSS(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateXSSServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateXSS),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
