70dacb5e629ea537ddc012e7b06cc85d4406aa67
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-10 16:32:51 +0700

    feat:upload_file_test add upload

--- upload_file_test.go
+++ upload_file_test.go
@@ -1,7 +1,9 @@
 package Golang_web
 
 import (
+	"io"
 	"net/http"
+	"os"
 	"testing"
 )
 
@@ -9,9 +11,36 @@ func UploadForm(w http.ResponseWriter, r *http.Request) {
 	myTemplates.ExecuteTemplate(w, "upload.form.gohtml", nil)
 }
 
+func Upload(w http.ResponseWriter, r *http.Request) {
+	//r.ParseMultipartForm(100<<200)  menetapkan masimum memori
+	file, fileheader, err := r.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+
+	//tujuan, lalu nama file
+	fileDestionation, err := os.Create("./resources/" + fileheader.Filename)
+	if err != nil {
+		panic(err)
+	}
+
+	//simpan file, ke destinasi file
+	_, err = io.Copy(fileDestionation, file)
+	if err != nil {
+		panic(err)
+	}
+	name := r.PostFormValue("name")
+	myTemplates.ExecuteTemplate(w, "upload.success.gohtml", map[string]interface{}{
+		"Name": name,
+		"File": "/static/" + fileheader.Filename,
+	})
+}
+
 func TestUploadForm(t *testing.T) {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/form", UploadForm)
+	mux.HandleFunc("/", UploadForm)
+	mux.HandleFunc("/upload", Upload)
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./resources"))))
 
 	server := http.Server{
 		Addr:    ("localhost:8080"),
